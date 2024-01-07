41c12e629eb0d87472c0b4886f196606d77ec0f3
Axel <axelcapuozzo@gmail.com>
2024-01-07 22:20:17 +0100

    Making Good pt. 1(wrote syntax tree generator)

--- tokenizer.py
+++ tokenizer.py
@@ -1,59 +1,103 @@
-import sys
+import os, time, sys, math
 
-out = []
-
-FUNC = "function"
-MODE_CHANGE = "mode_change"
-MATH_OP = "math_operation"
-DELIMITER = "delimiter"
-
-special = {
-    ";": DELIMITER,
-    "+": MATH_OP,
-    "-": MATH_OP,
-    "*": MATH_OP,
-    "/": MATH_OP,
-    "%": MATH_OP,
+start_time = time.time()
+height = 1
+ret_to_hight = 1
+tree = "[SyntaxTree"
+code = """if (1+1 == 2){
+    print(1, 123);
+    print("Hello World!");
+}"""
+buffer = ""
+reserved = {
+    "print": "Function",
+    ";": "eol",
+    "if": "IfStatement",
+    "+": "ArithmaticOperator",
+    "-": "ArithmaticOperator",
+    "*": "ArithmaticOperator",
+    "/": "ArithmaticOperator",
+    ",": "ArgumentSeparator",
+    "(": "ArgumentOpener",
+    ")": "ArgumentCloser",
+    "==": "ComparisonOperator",
+    "=!": "ComparisonOperator",
+    "==": "ComparisonOperator",
 }
-reserved_keywords = {"exit": FUNC, "print": FUNC}
+mode = ""
+for i in range(len(code)):
+    if code[i] != " ":
+        buffer += code[i]
+    print(buffer)
+    if buffer in reserved:
+        if reserved[buffer] == "IfStatement":
+            tree += "[IfStatement"
+            height += 1
+            ret_to_hight += 1
+        if reserved[buffer] == "Function":
+            height += 1
+            tree += "[Function " + buffer
+        if reserved[buffer] == "eol":
+            for j in range(height - ret_to_hight):
+                tree += "]"
+            height = ret_to_hight
+        if reserved[buffer] == "ArithmaticOperator":
+            tree += "[ArithmaticOperator: " + buffer + "]"
+        if reserved[buffer] == "ArgumentSeparator":
+            if code[::-1][0] == "]":
+                for j in range(height - ret_to_hight):
+                    tree += "]"
+                    height = ret_to_hight
+        if reserved[buffer] == "ArgumentOpener":
+            name_later = tree.split("[")[::-1][0].split(" ")[0]
+            tree += "["
+            if name_later == "IfStatement":
+                tree += "Condition"
+            elif name_later == "Function":
+                tree += "Arguments"
+            height += 1
+            ret_to_hight += 1
+        if reserved[buffer] == "ArgumentCloser":
+            tree += "]"
+            height -= 1
+            ret_to_hight -= 1
+        if reserved[buffer] == "ComparisonOperator":
+            tree += "[ComparisonOperator: " + buffer + "]"
+
+        buffer = ""
+    try:
+        if 0 <= i < len(code):  # HOW IS THIS NOT FUCKING WORKING
+            if code[i + 1] in [" ", ";", ","] or code[i + 1] in reserved:
+                if buffer.startswith('"'):
+                    start = i - (len(buffer) - 1)
+                    print(code[start])
+                    quotation_count = 0
+                    j = 0
+                    string_buffer = ""
+                    while quotation_count != 2:
+                        if code[start + j] != '"':
+                            string_buffer += code[start + j]
+                        else:
+                            quotation_count += 1
+                        j += 1
+                    tree += "[StringLiteral: " + string_buffer + "]"
+                elif buffer.isdigit():
+                    tree += "[IntegerLiteral: " + buffer + "]"
+                buffer = ""
+    except IndexError as e:
+        print("Out of index, handled")
+
+    if len(code) == i:
+        for j in range(ret_to_hight - 1):
+            tree += "]"
 
 
-def add(string):
-    if string in reserved_keywords:
-        out.append({"command": string, "type": reserved_keywords[string]})
-    elif string in special:
-        out.append({"command": string, "type": special[string]})
-    else:
-        out.append({"command": string, "type": "ambiguous"})
+print(tree)
+end_time = time.time()
+time = int(end_time) - int(start_time)
 
-
-def main(file_name, do_output=False, output_dir=""):
-    file = open(file_name, "r")
-    content = file.read()
-    file.close()
-
-    buffer = ""
-    for i in range(len(content)):
-        if content[i] == " " or content[i] == "\n" or content[i] in special:
-            if buffer != "":
-                add(buffer)
-            else:
-                pass  # TODO: should there be something here?
-
-            if content[i] in special:
-                add(content[i])
-
-            buffer = ""
-        else:
-            buffer += content[i]
-    # TODO: add way to save "compiled" code to a file
-
-    return out
-
-
-if __name__ == "__main__":
-    argv = sys.argv
-    if len(argv) < 2:
-        main("main.ehph")
-    else:
-        main(argv[1])
+print(
+    f"""
+Successfully parsed to 'FILEPLACEHOLD' in {time}s
+"""
+)
